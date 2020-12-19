5574895f8175edfaf849c20ff54ba64c472f3379
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2020-12-19 16:32:28 +0100

    :rocket: First commit ! :rocket:

---
+++ src/components/molecules/Nav/Nav.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Nav() {
+  return <nav>Je suis la nav</nav>;
+}
---
+++ src/components/organisms/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+  return <footer>Je suis le footer</footer>;
+}
