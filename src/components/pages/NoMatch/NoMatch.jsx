6df49998759fa40efca0fbc8564fc1ad546f662a
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2021-01-03 15:23:16 +0100

    :sparkles: Add no match :sparkles:

--- src/components/pages/NoMatch/NoMatch.jsx
+++ src/components/pages/NoMatch/NoMatch.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import "./NoMatch.scss";
 
 export default function NoMatch() {
   useEffect(() => {
@@ -9,5 +12,15 @@ export default function NoMatch() {
     }px))`;
   }, []);
 
-  return <div className="noMatch">Je suis la page no match !</div>;
+  return (
+    <div className="noMatch">
+      <h2 className="noMatch_oops">Oops...</h2>
+      <h3 className="noMatch_informations">
+        La page demandée n'est pas disponible actuellement !
+      </h3>
+      <Link to="/#" className="noMatch_button">
+        Retour à la page d'accueil
+      </Link>
+    </div>
+  );
 }
