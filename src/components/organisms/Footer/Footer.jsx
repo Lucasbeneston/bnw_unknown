04613b124c51111b5da772563f7d3dee48c6f147
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2021-01-04 11:32:08 +0100

    :recycle: Add dynamic date footer :recycle:

--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import "./Footer.scss";
 
 export default function Footer() {
+  const date = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <h5 className="footer_copyright">&copy; 2020 - bnw_unknown</h5>
+      <h5 className="footer_copyright">&copy; {date} - bnw_unknown</h5>
     </footer>
   );
 }
